8928d6b456896bdda907a10801d34286a0924452
Pacifique Linjanja <pacilinja2@gmail.com>
2026-06-16 09:46:18 +0200

    chore: improve skeletons

--- apps/website/components/MentorCardSkeleton.tsx
+++ apps/website/components/MentorCardSkeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from '@sos-academy/ui';
 
 export function MentorCardSkeleton() {
   return (
-    <div className="border border-white/5 h-40 bg-black/50 relative overflow-hidden">
-      <Skeleton className="absolute inset-y-0 left-0 w-[38%]" />
-      <div className="relative z-20 h-full p-3 pl-[36%] flex flex-col gap-2">
+    <div className="border border-white/5 h-40 bg-black/50 flex overflow-hidden">
+      <Skeleton className="w-[38%] flex-shrink-0" />
+      <div className="flex-1 p-3 flex flex-col gap-2.5">
         <Skeleton className="h-3.5 w-28" />
         <Skeleton className="h-2.5 w-20" />
         <Skeleton className="h-2.5 w-full" />
